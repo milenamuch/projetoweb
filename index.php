57ec4667ccecb9fa3cfa344c6a04966ebdc3fd6f
Milena Madeira <milenamuchenski@gmail.com>
2022-09-02 21:38:15 -0300

    projeto

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include "header.php";  
+?>
+ conteudo🤑
+
+ <?php
+    include "footer.php";
+?>
---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav class="main">
+<ul>
+    <li><a href="index.php">Inicio</a></li>
+    <li><a href="sobre.php">Sobre</a></li>
+    <li><a href="contato.php">Contato</a></li>
+</ul>
+
+</nav>
